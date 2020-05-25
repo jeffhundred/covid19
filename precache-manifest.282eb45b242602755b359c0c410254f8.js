9aa60bd0ae3f3d0baa8e2082ca25876686f5ab96
jeffhundred <benboyer128@gmail.com>
2020-05-25 00:20:20 -0400

    Updates

---
+++ precache-manifest.282eb45b242602755b359c0c410254f8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17b1ac5518724f2e0e052b0a8a64cd0d",
+    "url": "/covid19/index.html"
+  },
+  {
+    "revision": "96bb54e09f130a5f7324",
+    "url": "/covid19/static/css/main.d6ea9cb5.chunk.css"
+  },
+  {
+    "revision": "7793cf46b0fec289fde9",
+    "url": "/covid19/static/js/2.79151295.chunk.js"
+  },
+  {
+    "revision": "b4cabae32341920cfb4131dbdebda2c2",
+    "url": "/covid19/static/js/2.79151295.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96bb54e09f130a5f7324",
+    "url": "/covid19/static/js/main.4b1ccd64.chunk.js"
+  },
+  {
+    "revision": "3f9ea9de9b45ec0fb219",
+    "url": "/covid19/static/js/runtime-main.59095949.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/covid19/static/media/logo.5d5d9eef.svg"
+  }
+]);
